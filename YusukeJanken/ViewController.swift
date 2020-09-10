ecb22c3dfc6d6dadc20de6ade51dd09c3a9ba15d
Yu-suke441 <it.creative.pgm.738@gmail.com>
2020-09-10 20:07:45 +0900

    ジャンケンアプリを作る

--- YusukeJanken/ViewController.swift
+++ YusukeJanken/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerImageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shuffleAction(_ sender: Any) {
+    }
+    
 }
 
